97c8a1fd56cc999170641c423a7deffe16c60ab9
fadekoni <84129977+fadekoni@users.noreply.github.com>
2021-05-13 14:05:07 +0300

    Update küfür-engel.js

--- komutlar/küfür-engel.js
+++ komutlar/küfür-engel.js
@@ -3,20 +3,20 @@ const db = require('quick.db')
 
 exports.run = async(client, message, args) => {
   
-  if(!message.member.permissions.has("ADMINISTRATOR")) return message.channel.send(new Discord.MessageEmbed().setDescription("Bu Komutu Kullanabilmek İçin \`Yönetici\` Yetkisine Sahip Olmalısın !").setColor("RANDOM"))
+  if(!message.member.permissions.has("ADMINISTRATOR")) return message.channel.send(new Discord.MessageEmbed().setDescription("Bu komutu kullanabilmek için \`Yönetici\` yetkisine sahip olmalısın!").setColor("RANDOM"))
 
     if(args[0] === "aç") {
     db.set(`kfrengl_${message.guild.id}`, true)
-    message.channel.send(new Discord.MessageEmbed().setDescription("**Küfür engel sistemi başarılıyla açıldı!**").setColor("RANDOM"))
+    message.channel.send(new Discord.MessageEmbed().setDescription("**Küfür engel sistemi başarıyla açıldı!**").setColor("RANDOM"))
   }
   
   if(args[0] === "kapat") {
     db.delete(`kfrengl_${message.guild.id}`)
-    message.channel.send(new Discord.MessageEmbed().setDescription("**Küfür engel sistemi başarılıyla kapatıldı!**").setColor("RANDOM"))
+    message.channel.send(new Discord.MessageEmbed().setDescription("**Küfür engel sistemi başarıyla kapatıldı!**").setColor("RANDOM"))
   }
   
   if(!args[0]) {
-    message.channel.send(new Discord.MessageEmbed().setDescription("**\`aç\` veya \`kapat\` Yazmalısın ! Örnek Kullanım: !küfür-engel aç / kapat**").setColor("RANDOM"))
+    message.channel.send(new Discord.MessageEmbed().setDescription("**\`aç\` veya \`kapat\` yazmalısın! Örnek Kullanım: !küfür-engel aç / kapat**").setColor("RANDOM"))
   }
   
 }
