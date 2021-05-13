29dae5cf0bf33a3b9922f0f2f5e646b9c2327b3a
fadekoni <84129977+fadekoni@users.noreply.github.com>
2021-05-13 14:03:57 +0300

    Update emoji-koruma.js

--- komutlar/emoji-koruma.js
+++ komutlar/emoji-koruma.js
@@ -3,11 +3,11 @@ const db = require('quick.db')
 
 exports.run = async(client, message, args) => {
   
-  if(!message.member.permissions.has("ADMINISTRATOR")) return message.channel.send(new Discord.MesageEmbed().setDescription("Bu Komutu Kullanabilmek İçin \`Yönetici\` Yetkisine Sahip Olmalısın !").setColor("RANDOM"))
+  if(!message.member.permissions.has("ADMINISTRATOR")) return message.channel.send(new Discord.MesageEmbed().setDescription("Bu komutu kullanabilmek için \`Yönetici\` yetkisine sahip olmalısın!").setColor("RANDOM"))
   
   if(args[0] === "aç") {
     db.set(`emjkrm_${message.guild.id}`, true)
-    message.channel.send(new Discord.MesageEmbed().setDescription("**Emoji koruma sistemi başarılıyla açıldı!**").setColor("RANDOM"))
+    message.channel.send(new Discord.MesageEmbed().setDescription("**Emoji koruma sistemi başarıyla açıldı!**").setColor("RANDOM"))
   }
   
   if(args[0] === "kapat") {
@@ -16,7 +16,7 @@ exports.run = async(client, message, args) => {
   }
   
   if(!args[0]) {
-    message.channel.send(new Discord.MessageEmbed().setDescription("**\`aç\` veya \`kapat\` Yazmalısın ! Örnek Kullanım: !reklam-engel aç / kapat**").setColor("RANDOM"))
+    message.channel.send(new Discord.MessageEmbed().setDescription("**\`aç\` veya \`kapat\` yazmalısın! Örnek Kullanım: !reklam-engel aç / kapat**").setColor("RANDOM"))
   }
   
 }
