f9b79208594564292476af339befbc68cd77ffc6
fadekoni <84129977+fadekoni@users.noreply.github.com>
2021-05-13 14:00:06 +0300

    Update reklam-engel.js

--- komutlar/reklam-engel.js
+++ komutlar/reklam-engel.js
@@ -7,16 +7,16 @@ exports.run = async (client, message, args) => {
   
   if(args[0] === "aç") {
     db.set(`rklmengl_${message.guild.id}`, true)
-    message.channel.send(new Discord.MessageEmbed().setDescription("**Reklam Engel Sistemi Başarılı Şekilde Aktif Edildi !**").setColor("RANDOM"))
+    message.channel.send(new Discord.MessageEmbed().setDescription("**Reklam engel sistemi başarıyla açıldı!**").setColor("RANDOM"))
   }
   
   if(args[0] === "kapat") {
     db.delete(`rklmengl_${message.guild.id}`)
-    message.channel.send(new Discord.MessageEmbed().setDescription("**Reklam Engel Sistemi Başarıyla Kapatıldı !**").setColor("RANDOM"))
+    message.channel.send(new Discord.MessageEmbed().setDescription("**Reklam engel sistemi başarıyla kapatıldı!**").setColor("RANDOM"))
   }
   
   if(!args[0]) {
-    message.channel.send(new Discord.MessageEmbed().setDescription("**\`aç\` veya \`kapat\` Yazmalısın ! Örnek Kullanım; !reklam-engel aç / kapat**").setColor("RANDOM"))
+    message.channel.send(new Discord.MessageEmbed().setDescription("**\`aç\` veya \`kapat\` yazmalısın! Örnek Kullanım: !reklam-engel aç / kapat**").setColor("RANDOM"))
   }
   
 }
